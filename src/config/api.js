e74f0a499e7b147f145a81b0968d24bf9c256469
Mandar Prabhu <manpra3007@gmail.com>
2026-06-25 01:17:07 +0530

    fix: reset cache, proper gitignore

--- src/config/api.js
+++ src/config/api.js
@@ -1,1 +1,1 @@
-export const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
+export const BASE_URL = import.meta.env.VITE_API_URL || 'http://https://queuecure-a2vo.onrender.com';
